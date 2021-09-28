dfe52bfc55da01913fe6fb04e1a06d1e76c50c3f
Erasmo J.F Da Silva <erasmogohiper@gmail.com>
2021-09-28 17:24:17 -0300

    CM - /Let's design the layout of the header in Xcode
    Let's design the layout of the header in Xcode

--- abacate/abacate/View/ContentView.swift
+++ abacate/abacate/View/ContentView.swift
@@ -11,8 +11,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Avocado Recipes")
-            .padding()
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack(alignment: .center,spacing: 20) {
+                // MARK: - HEADER
+                ScrollView(.horizontal,showsIndicators: false) {
+                HStack(alignment: .top) {
+                    HeaderView()
+                   }
+                }
+                
+                // MARK: - FOOTER
+                VStack(alignment: .center, spacing: 20) {
+                    Text("All about avocados")
+                        .font(.system(.title, design: .serif))
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("ColorGreenAdaptive"))
+                        .padding(8)
+                    Text("Everything you wanted to know about avocados but weretoo afraid do ask.")
+                        .font(.system(.body, design: .serif))
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(Color.gray)
+                    
+                }//VStack
+                .frame(maxWidth: 640)
+                .padding()
+                .padding(.bottom, 85)
+            }//VStack
+        }//ScrollView
+        .edgesIgnoringSafeArea(.all)
+        .padding(0)
     }
 }
 
---
+++ abacate/abacate/View/HeaderView.swift
@@ -0,0 +1,68 @@
+//
+//  HeaderView.swift
+//  abacate
+//
+//  Created by Erasmo J.F Da Silva on 27/09/21.
+//  SwiftUI ♡ Better Apps. Less Code
+//  https://erasmojf.github.io/
+//  Fidju de Bideira de Feira de Caracol
+
+import SwiftUI
+
+struct HeaderView: View {
+    // MARK: - PROPERTIES
+    @State private var showHeadline: Bool = false
+    var slideAnimation: Animation {
+        Animation.spring(response: 1.5, dampingFraction: 0.5, blendDuration: 0.5)
+            .speed(1)
+            .delay(0.25)
+    }
+    
+    var body: some View {
+        ZStack {
+            Image("avocado-slice-1")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+            
+            HStack(alignment: .top, spacing: 0) {
+                Rectangle()
+                    .fill(Color("ColorGreenLight"))
+                    .frame(width: 4)
+                
+                VStack(alignment: .leading, spacing: 6) {
+                    Text("Avocado")
+                        .font(.system(.title,design: .serif))
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.white)
+                        .shadow(radius: 3)
+                    
+                    Text("Avocados are a powerhouse ingredient at any meal for anyone.")
+                        .font(.footnote)
+                        .lineLimit(2)
+                        .multilineTextAlignment(.leading)
+                        .foregroundColor(Color.white)
+                        .shadow(radius: 3)
+                    
+                }//VStack
+                .padding(.vertical, 0)
+                .padding(.horizontal, 20)
+                .frame(width: 281, height: 105)
+                .background(Color("ColorBlackTransparentLight"))
+            }//HStack
+            .frame(width: 285, height: 105, alignment: .center)
+            .offset(x: -66, y: showHeadline ? 75 : 220)
+            .animation(slideAnimation)
+            .onAppear(perform: {
+                self.showHeadline.toggle()
+            })
+        }//ZStack
+        .frame(width: 480, height: 320, alignment: .center)
+    }
+}
+
+struct HeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        HeaderView()
+            .previewLayout(.sizeThatFits)
+    }
+}
