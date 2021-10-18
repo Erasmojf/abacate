7d6013a62e4c2a370a9c16e1d01dffb6a7510b8d
Erasmo J.F Da Silva <erasmogohiper@gmail.com>
2021-10-18 16:07:38 -0300

    CM - How to Build a Basic Form UI for iOS with SwiftUI
    How to Build a Basic Form UI for iOS with
    SwiftUI

--- abacate/abacate/View/SettingsView.swift
+++ abacate/abacate/View/SettingsView.swift
@@ -10,9 +10,89 @@
 import SwiftUI
 
 struct SettingsView: View {
+    // MARK: - PROPERTIES
+    @State private var enableNotification: Bool = true
+    @State private var backroundRefresh: Bool = false
+    
     var body: some View {
-        Text("Settings")
-    }
+        VStack(alignment: .center, spacing: 0) {
+            // MARK: - HEADER
+            VStack(alignment: .center, spacing:  5) {
+                Image("avocado")
+                    .resizable()
+                    .scaledToFit()
+                    .padding(.top)
+                    .frame(width: 100, height: 100, alignment: .center)
+                    .shadow(color: Color("ColorGreenLight"), radius: 8, x: 0, y: 4)
+                Text("Avocados".uppercased())
+                    .font(.system(.title, design: .serif))
+                .foregroundColor(Color("ColorGreenAdaptive"))
+            }
+            .padding()
+            Form {
+                // MARK: - Section #1
+                Section(header: Text("General Settings")){
+                    Toggle(isOn: $enableNotification) {
+                    Text("Enable notification")
+                  }//Toggle
+                    Toggle(isOn: $backroundRefresh) {
+                        Text("Background refresh")
+                    }
+                }
+                Section(header: Text("Application")) {
+                    if enableNotification {
+                        HStack {
+                            Text("Product")
+                                .foregroundColor(Color.gray)
+                            Spacer()
+                            Text("Avocado Recipes")
+                        }
+                        HStack {
+                            Text("Compatibility")
+                                .foregroundColor(Color.gray)
+                            Spacer()
+                            Text("iPhone & iPad")
+                            
+                        }//HStack
+                        HStack {
+                            Text("Developer")
+                                .foregroundColor(Color.gray)
+                            Spacer()
+                            Text("Maia / Erasmo")
+                            
+                        }//HStack
+                        HStack {
+                            Text("Designer")
+                                .foregroundColor(Color.gray)
+                            Spacer()
+                            Text("Inocencia")
+                        }//HStack
+                        HStack {
+                            Text("Website")
+                                .foregroundColor(Color.gray)
+                            Spacer()
+                            Text("Erasmojf.io")
+                        }//HStack
+                        HStack {
+                            Text("Version")
+                                .foregroundColor(Color.gray)
+                            Spacer()
+                            Text("1.0.0")
+                        }//HStack
+                    } else {
+                        HStack {
+                            Text("Personal message")
+                                .foregroundColor(Color.gray)
+                            Spacer()
+                            Text("Happy Coding")
+                        }
+                    }//HStack
+                    
+                }
+            }//Form
+        }//VStack
+        .frame(maxWidth: 640)
+    }//VStack
 }
 
 struct SettingsView_Previews: PreviewProvider {
